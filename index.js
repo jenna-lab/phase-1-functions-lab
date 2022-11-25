eb424ca142d5b4787ca5e5d7bdfc72a7b598f0ed
jenna-lab <jennifer.sammy@student.moringaschool.com>
2022-11-25 10:44:08 +0300

    completed assignment

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ function distanceFromHqInBlocks(block) {
     return block - hq;
   }
 }
-function distanceFromHqInFeet(someValue) {
-  return distanceFromHqInBlocks(someValue) * 264;
+function distanceFromHqInFeet(feet) {
+  return distanceFromHqInBlocks(feet) * 264;
   // calls distanceFromHqInBlocks from inside the distanceFromHqInFeet function
   //passing the argument from distanceFromHqInFeet into distanceFromHqInBlocks
   //the return value of distanceFromHqInBlocks can then be used to calculate feet
